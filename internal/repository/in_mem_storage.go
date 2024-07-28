8f83cd63e4d2287846de808a32adbf99e2ce454a
Vladislav D <dvs@crcr.ru>
2024-07-28 12:04:17 +0300

    fix datarace

--- internal/repository/in_mem_storage.go
+++ internal/repository/in_mem_storage.go
@@ -1,81 +1,81 @@
 package repository
 
 import (
-	"sync"
+    "sync"
 
-	"m5s/domain"
+    "m5s/domain"
 )
 
 type InMemStorage struct {
-	sync.RWMutex
-	store map[string]*domain.Metric
+    sync.RWMutex
+    store map[string]*domain.Metric
 }
 
 func NewInMemStorage() *InMemStorage {
-	return &InMemStorage{
-		store: make(map[string]*domain.Metric),
-	}
+    return &InMemStorage{
+        store: make(map[string]*domain.Metric),
+    }
 }
 
 func (ims *InMemStorage) Update(metric *domain.Metric) error {
-	ims.RLock()
-	defer ims.RUnlock()
+    ims.Lock()
+    defer ims.Unlock()
 
-	if metric.Type == domain.MetricTypeCounter {
-		newValue := metric.IntValue
+    if metric.Type == domain.MetricTypeCounter {
+        newValue := metric.IntValue
 
-		prevMetric, ok := ims.store[metric.Name]
-		if ok {
-			newValue += prevMetric.IntValue
+        prevMetric, ok := ims.store[metric.Name]
+        if ok {
+            newValue += prevMetric.IntValue
 
-			metric.IntValue = newValue
+            metric.IntValue = newValue
 
-			ims.store[metric.Name] = metric
+            ims.store[metric.Name] = metric
 
-			return nil
-		}
-	}
+            return nil
+        }
+    }
 
-	ims.store[metric.Name] = metric
+    ims.store[metric.Name] = metric
 
-	return nil
+    return nil
 }
 
 func (ims *InMemStorage) GetMetric(
-	metricType domain.MetricType,
-	name string,
+    metricType domain.MetricType,
+    name string,
 ) (*domain.Metric, error) {
-	ims.RLock()
-	defer ims.RUnlock()
+    ims.RLock()
+    defer ims.RUnlock()
 
-	if name == "" {
-		return nil, domain.ErrInvalidMetricName
-	}
+    if name == "" {
+        return nil, domain.ErrInvalidMetricName
+    }
 
-	metric, ok := ims.store[name]
-	if !ok {
-		return nil, domain.ErrNoSuchMetric
-	}
+    metric, ok := ims.store[name]
+    if !ok {
+        return nil, domain.ErrNoSuchMetric
+    }
 
-	if metric.Type != metricType {
-		return nil, domain.ErrInvalidMetricType
-	}
+    if metric.Type != metricType {
+        return nil, domain.ErrInvalidMetricType
+    }
 
-	return metric, nil
+    return metric, nil
 }
 
 func (ims *InMemStorage) GetMetricsList() []*domain.Metric {
-	ims.RLock()
-	defer ims.RUnlock()
+    ims.RLock()
+    defer ims.RUnlock()
 
-	metrics := make([]*domain.Metric, len(ims.store))
+    metrics := make([]*domain.Metric, len(ims.store))
 
-	i := 0
+    i := 0
 
-	for _, metric := range ims.store {
-		metrics[i] = metric
-		i++
-	}
+    for _, metric := range ims.store {
+        metrics[i] = metric
+        i++
+    }
 
-	return metrics
+    return metrics
 }
